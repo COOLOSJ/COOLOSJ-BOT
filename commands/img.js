e1a01a6c5fd92b7418238f18366c2b82fc59c0af
COOLOSJ <126161496+COOLOSJ@users.noreply.github.com>
2026-03-13 14:08:46 +0000

    Added Pexels image search so img command actually works

--- commands/img.js
+++ commands/img.js
@@ -1,27 +1,46 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+const PEXELS_KEY = process.env.PEXELS_KEY;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('img')
-        .setDescription('Send a random image')
+        .setDescription('Search for an image')
         .addStringOption(option =>
             option.setName('prompt')
-                .setDescription('Image topic')
+                .setDescription('Image to search')
                 .setRequired(true)
         ),
 
     async execute(interaction) {
 
+        await interaction.deferReply();
+
         const prompt = interaction.options.getString('prompt');
 
-        const imageUrl = `https://picsum.photos/800/600`;
+        try {
 
-        const embed = new EmbedBuilder()
-            .setTitle(`Image result for "${prompt}"`)
-            .setImage(imageUrl)
-            .setColor(0x00AE86);
+            const response = await axios.get(
+                `https://api.pexels.com/v1/search?query=${prompt}&per_page=1`,
+                {
+                    headers: { Authorization: PEXELS_KEY }
+                }
+            );
 
-        await interaction.reply({ embeds: [embed] });
+            const image = response.data.photos[0].src.large;
 
+            const embed = new EmbedBuilder()
+                .setTitle(`Image result for "${prompt}"`)
+                .setImage(image)
+                .setColor(0x00AE86);
+
+            await interaction.editReply({ embeds: [embed] });
+
+        } catch (error) {
+
+            await interaction.editReply("Couldn't find an image.");
+
+        }
     }
 };
